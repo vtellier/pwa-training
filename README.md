b1c4bc2866e93112d153487a4d78d0f0cab5fba4
Vincent Tellier <vincent.lt.tellier@gmail.com>
2020-11-23 11:37:31 +0000

    Started to play with the service worker

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pwa-training
+
 My training sessions about progessive web apps.
+See [my website for more details](https://vte.io/2020/11/toward-progressive-web-apps/)
+
+The idea here is to practice with the basics of _progessive web apps_ without any framework or fancy boiler plate.
+
+For this, I use only plain #HTML and #Javascript.
+
+
+
---
+++ my-service-worker.js
@@ -0,0 +1,29 @@
+self.addEventListener('install', e => {
+    console.log('install event:', e);
+    // Here we should install all the elements we'll need to use the app offline.
+    e.waitUntil(
+        new Promise((resolve, reject) => {
+            setTimeout(() => { resolve() }, 3000 );
+        }));
+});
+self.addEventListener('activate', e => {
+    console.log('activate event:', e);
+    // Here we should clean all ressources from the previous versions we don't need anymore.
+});
+self.addEventListener('fetch', e => {
+    console.log('fetch', e);
+    let url = e.request.url;
+    console.log(url);
+    if(url == "http://127.0.0.1:8080/uhhh.html") {
+        console.log('Hahaha i am maning in the middle');
+        e.respondWith(fetch('http://127.0.0.1:8080/hello.txt'));
+    }
+    else if(url == "http://127.0.0.1:8080/sw/version") {
+        console.log('giving version of this SW');
+        const response = new Response();
+        response.body = "I am the version 1.0.0";
+        e.respondWith(response);
+    }
+    else
+        e.respondWith(fetch(url));
+});
