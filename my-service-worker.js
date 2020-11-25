876efd382237a3653511ca90f4a2a5b6c9b6bc06
Vincent Tellier <vincent.lt.tellier@gmail.com>
2020-11-25 14:26:12 +0000

    Added some resources and caching policy at installation

--- my-service-worker.js
+++ my-service-worker.js
@@ -1,11 +1,18 @@
-const version = "1.0.0";
+const version = "1.1.0";
+const cacheKey = "pwa-training-cache-1.1.0";
+
+const toCache = [
+    "/img/punta-da-piedade.jpg",
+    "hello.txt"
+];
+
 self.addEventListener('install', e => {
     console.log('install event:', e);
     // Here we should install all the elements we'll need to use the app offline.
     e.waitUntil(
-        new Promise((resolve, reject) => {
-            setTimeout(() => { resolve() }, 3000 );
-        }));
+        caches.open(cacheKey)
+            .then(cache => { return cache.addAll(toCache); })
+    );
 });
 self.addEventListener('activate', e => {
     console.log('activate event:', e);
@@ -24,6 +31,54 @@ self.addEventListener('fetch', e => {
         const response = new Response(version, { status: 200 });
         e.respondWith(response);
     }
-    else
+    else if(url == "http://127.0.0.1:8080/sw/caches.keys") {
+        e.respondWith(new Promise((resolve, reject) => {
+            caches.keys().then(keys => {
+                const str = 
+                resolve(new Response("<p>List of caches:</p><ul>"
+                    + keys.reduce((acc,curr) =>  { return acc+'<li>'+curr+'</li>' },"") + "</ul>", { status: 200 }));
+            })
+            .catch(err => {
+                resolve(new Response(err, { status: 500 }));
+            });
+        }));
+    }
+    else if(url == "http://127.0.0.1:8080/img/oya.jpg") {
+        e.respondWith(new Promise((resolve,reject) => {
+            caches.match(e.request)
+            .then(r => {
+                if(r === undefined) {
+                    console.log("Øya is not in the cache! Let's put it into a default one.");
+                    caches.open('default') // arbitrary name
+                        .then(cache => {
+                            cache.add(e.request)
+                                .then(() => {
+                                    caches.match(e.request)
+                                    .then(r => { resolve(r); });
+                                })
+                                .catch(err => {
+                                    console.log("Here's the error I got while trying to add the requested thing to the cache:", err);
+                                });
+                        })
+                        .catch(() => { resolve(new Response("Got a problem somewhere...", { status: 404 })); });
+                }
+                else {
+                    console.log("Here's Øya's pic from the cache:", r);
+                    resolve(r);
+                }
+            })
+        }));
+    }
+    else {
+        caches.match(e.request)
+            .then(r => {
+                if(r === undefined) {
+                    console.log("Not cached");
+                    return fetch(e.request);
+                }
+                else
+                    return r;
+            });
         e.respondWith(fetch(url));
+    }
 });
