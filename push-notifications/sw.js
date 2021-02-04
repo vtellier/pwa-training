9678c337526d1b38a5caae4e60b11c2e15845884
Vincent Tellier <vincent.lt.tellier@gmail.com>
2021-02-04 20:10:15 +0000

    Just to commit

--- push-notifications/sw.js
+++ push-notifications/sw.js
@@ -2,6 +2,10 @@ self.addEventListener('install', (...args) => {
     console.log('install', ...args);
 });
 
-self.addEventListener('notificationclick', (...args) => {
-    console.log('notificationclic', ...args);
+//self.addEventListener('notificationclick', (...args) => {
+//    console.log('notificationclic', ...args);
+//});
+self.addEventListener('notificationclick', function(event) {
+  console.log('On notification click: ', event.notification.tag);
+  event.notification.close();
 });
